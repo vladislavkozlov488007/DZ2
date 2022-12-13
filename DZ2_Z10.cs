13b7c242bdd7edc6f1363db875eeb2a8e4f52a0c
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2022-12-13 10:45:27 +0300

    Add files via upload

---
+++ DZ2_Z10.cs
@@ -0,0 +1,11 @@
+﻿int InputSecondNumber(int num)
+{
+    int count;
+    count = num % 100 / 10;
+    return count;
+}
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SecondNumber = InputSecondNumber(num);
+Console.Write($"вторая цифра от числа {num} это цифра  {SecondNumber}");
